edd9fd2978413a537078762527ac8a1c498f7a52
Evan Novak <evannvk@gmail.com>
2020-03-01 14:59:23 -0500

    Added description drop down

--- js/app.js
+++ js/app.js
@@ -48,3 +48,18 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
